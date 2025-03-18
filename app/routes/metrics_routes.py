5ab465209b596db38d0d6eac000a030b179b0d60
Spokay <hugobressange@gmail.com>
2025-03-18 10:36:33 +0100

    added the endpoint to expose prometeus metrics and a method to update the custom CPU / GPU / Memory metrics

--- app/routes/metrics_routes.py
+++ app/routes/metrics_routes.py
@@ -1,26 +1,33 @@
-from prometheus_client import Gauge
+import psutil
+from fastapi import APIRouter
+from prometheus_client import Gauge, generate_latest, REGISTRY, CONTENT_TYPE_LATEST
 from pynvml import *
-
-from app.config import logger
+from starlette.responses import Response
 
 CPU_USAGE = Gauge('process_cpu_usage', 'Current CPU usage in percent')
 GPU_USAGE = Gauge('process_gpu_usage', 'Current GPU usage in percent')
 MEMORY_USAGE = Gauge('process_memory_usage_bytes', 'Current memory usage in bytes')
 
 
-async def start_nvml():
-    device = nvmlDeviceGetHandleByIndex(0)
-    try:
-        logger.info("Initializing nvml")
-        nvmlInit()
-
-        logger.info('Started with Nvidia driver version = %s', nvmlSystemGetDriverVersion())
-
-    except Exception as e:
-        logger.info('Exception thrown - %s', e, exc_info=True)
-
-    finally:
-        nvmlShutdown()
+router = APIRouter()
 
 def update_system_metrics():
-    pass
+    devices = nvmlDeviceGetCount()
+
+    for i in range(devices):
+        handle = nvmlDeviceGetHandleByIndex(i)
+        utilization = nvmlDeviceGetUtilizationRates(handle)
+        memory_info = nvmlDeviceGetMemoryInfo(handle)
+
+        GPU_USAGE.set(utilization.gpu)
+        CPU_USAGE.set(psutil.cpu_percent())
+        MEMORY_USAGE.set(memory_info.used)
+
+
+@router.get(
+    "/metrics",
+    description="Endpoint to retrieve prometeus metrics",
+)
+async def metrics():
+    update_system_metrics()
+    return Response(generate_latest(REGISTRY), media_type=CONTENT_TYPE_LATEST)
